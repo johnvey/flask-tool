9b77e4555b7ce89aa1ad9428686e70bc91570aa8
J Hwang <go.github@johnvey.com>
2012-09-11 01:52:24 -0700

    updating version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Tool',
-    version='0.1.1',
+    version='0.1.2',
     url='https://github.com/johnvey/flask-tool',
     license='BSD',
     author='J Hwang',
